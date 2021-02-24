fea5727bf9cd5c0e1cd6ac6fe3fe3cfc7dd20151
Bandhan singh <parbhatbandhan@gmail.com>
2021-02-24 18:02:31 +0530

    Update knapsack assign.cpp

--- DAA PRACTICAL/knapsack assign.cpp
+++ DAA PRACTICAL/knapsack assign.cpp
@@ -2,45 +2,38 @@
 # include<stdio.h>
 void knapsack(int n, float weight[], float profit[], float capacity)
  { 
-   float x[20], tw = 0;
+   float x[20], tp = 0;
    int i, j, u;
    u = capacity;
+   for (i = 0; i < n; i++) x[i] = 0.0;
+   
    for (i = 0; i < n; i++)
-   {
-      x[i] = 0.0;
-    }
-    
-    for (i = 0; i < n; i++)
     { 
-	   if (profit[i] > u)
-       {
-	   break;
-       }
-      else 
+	  if (weight[i] > u)
+      break;
+    
+	  else 
 	  {
       x[i] = 1.0;
-      tw = tw + weight[i];
-	  u = u - profit[i];
+      tp = tp + profit[i];
+	  u = u - weight[i];
 	  }
     }
     if (i < n)
-    x[i] = u / profit[i];
-    tw = tw + (x[i] * weight[i]); 
+    x[i] = u / weight[i];
+    tp = tp + (x[i] * profit[i]); 
      printf("\nThe result vector is:- ");
 
     for (i = 0; i < n; i++)
-    {
-	printf("%f\t", x[i]); 
-    }
-	
-	printf("\nMaximum weight is:- %f", tw);
+    printf("%f\t", x[i]); 
+    printf("\nMaximum profit is:- %f", tp);
     
 	}
 
 
  int main() 
  {
-  float weight[20], profit[20], capacity;  // capacity = profit capacity
+  float weight[20], profit[20], capacity;
   int num, i, j;
   float ratio[20], temp;
 
@@ -53,24 +46,17 @@ void knapsack(int n, float weight[], float profit[], float capacity)
     scanf("%f %f", &weight[i], &profit[i]);
   }
  
-  printf("\nEnter the profit capacity of knapsack:- "); 
+  printf("\nEnter the capacity of knapsack:- "); 
   scanf("%f", &capacity);
-    for (i = 0; i < num; i++)
-	 { 
-	 ratio[i] = profit[i] / weight[i];
-	 }
+    
 	 
     for (i = 0; i < num; i++)
 	 {
       for (j = i + 1; j < num; j++)
 	  {
-	    if (ratio[i] < ratio[j]) 
+	    if (profit[i] < profit[j]) 
 	    {
-          temp = ratio[j];
-	      ratio[j] = ratio[i]; 
-	      ratio[i] = temp;
-	     
-		  temp = weight[j]; 
+	      temp = weight[j]; 
           weight[j] = weight[i];
 		  weight[i] = temp; 
 		 
@@ -80,7 +66,6 @@ void knapsack(int n, float weight[], float profit[], float capacity)
 		   }
 		   }
 		   }
-		   
-       knapsack(num, weight, profit, capacity);
+	   knapsack(num, weight, profit, capacity);
        return(0);
  }
